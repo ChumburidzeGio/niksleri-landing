d887fbb67fbdc1f7a5514ffa228c1686dcf218dd
Giorgi Chumburidze <giorgi@esflow.io>
2025-08-04 15:51:31 +0200

    add relocify logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,26 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <Image
           className="dark:invert"
-          src="/logo.png"
+          src="/icon.svg"
           alt="Niksleri logo"
           width={180}
           height={38}
           priority
         />
-        <h1 className="text-3xl font-extrabold">ნიქსლერი</h1>
+        <h1 className="text-3xl font-extrabold">ნიქსლერი / Relocify</h1>
         <p className="text-sm/6 text-center font-[family-name:var(--font-geist-mono)] max-w-3xl">
-          Niksleri LLC is a Georgian software studio. We design, ship and maintain the engine behind the remote-rental marketplace, Relocify.
+          Niksleri LLC operating under the brand "Relocify" is a Georgian software developer. We design, ship and maintain the engine behind the remote-rental marketplace, Relocify.
         </p>
 
-        {/*<div className="flex gap-4 items-center flex-col sm:flex-row">*/}
-        {/*  <a*/}
-        {/*      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"*/}
-        {/*    href="https://www.relocify.nl/"*/}
-        {/*    rel="noopener noreferrer"*/}
-        {/*  >*/}
-        {/*    Relocify →*/}
-        {/*  </a>*/}
-        {/*</div>*/}
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://www.relocify.nl/"
+            rel="noopener noreferrer"
+          >
+            Relocify Netherlands →
+          </a>
+        </div>
       </main>
     </div>
   );
